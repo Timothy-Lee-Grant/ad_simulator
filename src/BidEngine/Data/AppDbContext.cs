2bb19e7fea80b109dc653c263927bf00e95e3554
Timothy Grant <timothyleegrant1995@gmail.com>
2025-12-08 22:38:33 -0800

    Finished modeling the database entities on the C# side of things.

--- src/BidEngine/Data/AppDbContext.cs
+++ src/BidEngine/Data/AppDbContext.cs
@@ -19,13 +19,47 @@ public class AppDbContext : DbContext
         modelBuilder.Entity<Campaign>(entity =>
         {
             entity.HasKey(e => e.Id);
+            entity.Property(e=>e.Name).IsRequired();
+            entity.Property(e=>e.Status).IsRequired();
+            entity.Property(e=>e.CpmBid).HasColumnType("numeric(10,4)");
+            entity.Property(e=>e.DailyBudget).HasColumnType("numeric(12,2)");
+            entity.Property(e=>e.SpentToday).HasColumnType("numeric(12,2)");
 
             entity.HasMany(e => e.Ads)
                 .WithOne(e => e.Campaign)
                 .HasForeignKey(e => e.CampaignId);
 
+            entity.HasMany(e => e.TargetingRules)
+                .WithOne(e => e.Campaign)
+                .HasForeignKey(e => e.CampaignId);
+
             entity.HasIndex(e => e.Status);
-        }) 
+            entity.HasIndex(e => e.AdvertiserId);
+        }); 
+
+        modelBuilder.Entity<Ad>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            //entity.Property(e=>e.CampainId).IsRequired(); //why is this not required? because in the database schema it is listed as required, but the example doesn't have it as reqired??
+            entity.Property(e=>e.Title).IsRequired();
+            entity.Property(e=>e.RedirectUrl).IsRequired();
+            entity.Property(e=>e.ImageUrl).IsRequired();
+
+            entity.HasIndex(e=>e.CampaignId);
+        });
+
+        modelBuilder.Entity<TargetingRules>(entity=>
+        {
+            entity.HasKey(e=>e.Id);
+            //entity.Property(e=>e.CampaignId).IsRequired();
+            entity.Property(e=>e.RuleType).IsRequired();
+            entity.Property(e=>e.RuleValue).IsRequired();
+
+            entity.HasIndex(e=>e.CampaignId);
+            entity.HasIndex(e => new { e.
+            CampaignId, e.RuleType, e.RuleValue}).
+            IsUnique();
+        });
     }
 
 
