a5e583f63671e03ad436cc665418be92b2994db7
Timothy Grant <timothyleegrant1995@gmail.com>
2025-12-12 15:37:58 -0800

    Added the high level program.cs code to be able to start up application.

--- src/BidEngine/Program.cs
+++ src/BidEngine/Program.cs
@@ -1,3 +1,63 @@
+
+using BidEngine.Data;
+using BidEngine.Services;
+using Microsoft.EntityFrameworkCore;
+using Prometheus;
+using StackExchange.Redis;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//add services to the container
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+//add entity framework
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+
+
+//add redis
+var redisConnectionString = builder.Configuration.GetConnectionString("Redis") ?? "redis:6379";
+var redis = ConnectionMultiplexer.Connect(redisConnectionString);
+builder.Services.AddSingleton(redis);
+
+//add custom services
+builder.Services.AddScoped<CampaignCashe>();
+builder.Services.AddScoped<BidSelector>();
+builder.Services.AddScoped<BudgetService>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Database migration (optional - can also use SQL scripts)
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    // db.Database.Migrate(); // Uncomment if using EF Core migrations
+}
+
+//configure the http request pipeline
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//prometheus metrics endpoint
+app.UseRouting();
+app.useEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapMetrics();
+});
+
+app.Run();
+
+
+
+
+/*
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -39,3 +99,4 @@ record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
+*/
