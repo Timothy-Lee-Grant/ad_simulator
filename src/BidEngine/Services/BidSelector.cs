ad24889ca6f3c1d751fbb868e37369d83d60e59d
Timothy Grant <timothyleegrant1995@gmail.com>
2025-12-18 12:30:16 -0800

    This was my initial meth of SelectWinningBidAsyncAlgorithm2. It does not work, but I want to show the initial thought process.

--- src/BidEngine/Services/BidSelector.cs
+++ src/BidEngine/Services/BidSelector.cs
@@ -30,10 +30,28 @@ public class BidSelector
         _logger = logger;
     }
 
+    public async Task<BidResponse?> SelectWinningBidAsync(BidRequest request)
+    {
+        var adSelection = 0.1;
+        //var res = null;
+        BidResponse? res;
+        if(adSelection>0.5)
+        {
+            res = await SelectWinningBidAsyncAlgorithm1(request);
+        }
+        else
+        {
+            res = await SelectWinningBidAsyncAlgorithm2(request);
+        }
+        
+
+        return res;
+    }
+
     /// <summary>
     /// Select the winning campaign based on bid price and targeting match
     /// </summary>
-    public async Task<BidResponse?> SelectWinningBidAsync(BidRequest request)
+    public async Task<BidResponse?> SelectWinningBidAsyncAlgorithm1(BidRequest request)
     {
         _logger.LogInformation(
             "Evaluating bids for user {UserId} on placement {PlacementId}",
@@ -124,6 +142,89 @@ public class BidSelector
             },
             Confidence = 0.95 // this will need to be updated later to match real targeting metrics
         };
+
+        return response;
+    }
+
+/*
+This function will be called by the http rest controller when the client attempts to 
+request an ad from our service.
+Inputs: request will be the body of the http resquest which comes from the user.
+Outputs: A newly created BidResponse which will signify the 'highest' priced advertisement
+which our algorithm has found. The function will fill out the fields of a BidResponse with that
+advertisement's information and return it back.
+*/
+    public async Task<BidResponse?> SelectWinningBidAsyncAlgorithm2(BidRequest request)
+    {
+        _logger.LogInformation(
+            "Evaluating bids for user {UserId} on placement {PlacementId}",
+            request.UserId,
+            request.PlacementId
+        );
+
+        //activeCampaigns will be a List<Campaigns> which are active
+        //Tim Grant - As of now we have GetActiveCampaignsAsync() declared as List<Campaigns> , but what if there are no campaigns to serve? then we should get a null value???
+        var activeCampaigns = await _cashe.GetActiveCampaignsAsync();
+
+        if(activeCampaigns == null)
+        {
+            return null;
+        }
+
+
+        //My task now is to find highest advertisement which I am hoping to select and give to the user
+        //Tim Grant This will be my 'algorithm' for selection
+        //could use greedy algorithm where I first select the best campaign and then select the best ad from that best campaign 
+        //other idea is just do a test and select a random ad, then develop greedy later (because I would need to add multipliers to my ad dataset)
+        var winningCampaign = activeCampaigns[_random.Next(0, activeCampaigns.Count)];
+
+        //check if winningCampaign is null
+        if(winningCampaign == null)
+        {
+            _logger.LogWarning("SelectWinningBidAsyncAlgorithm2 was unable to find winningCampaign and got a NULL back");
+            return null;
+        }
+        if(winningCampaign.Ads.Count == 0)
+        {
+            _logger.LogWarning("SelectWinningBidAsyncAlgorithm2 winningCampaign has Ads count of 0");
+            return null;
+        }
+        //Now that I have a winning campaign. I need to select a winning bid
+        var winningBid = winningCampaign.Ads[_random.Next(0, winningCampaign.Ads.Count)];
+
+        /*
+        public class BidResponse
+        {
+            public Guid CampaignId {get; set;}
+            public Guid AdId {get; set;}
+            public AdContent AdContent  {get; set;} = null!;
+            public decimal BidPrice  {get; set;}
+            public double Confidence {get; set;}
+
+        }
+        */
+        var response = new BidResponse();
+        response.CampaignId = winningCampaign.Id;
+        response.AdId = winningBid.Id;
+        response.AdContent = new AdContent();
+        response.BidPrice = winningCampaign.CpmBid / 1000;
+        response.Confidence = 0.5; //This has not been implemented. I would need some algorithm to determine what confidence I have and use my metrics to determine this
+        
+        /*
+        public class AdContent
+        {
+            public string ImageUrl {get; set;} = string.Empty;
+            public string Title {get; set;} = string.Empty;
+            public string RedirectUrl {get; set;} = string.Empty;
+        }
+
+        public string Title {get; set;} = string.Empty;
+        public string ImageUrl {get; set;} = string.Empty;
+        public string RedirectUrl {get; set;} = string.Empty;
+        */
+        response.AdContent.ImageUrl = winningBid.ImageUrl;
+        response.AdContent.Title = winningBid.Title;
+        response.AdContent.RedirectUrl = winningBid.RedirectUrl;
 
         return response;
     }
